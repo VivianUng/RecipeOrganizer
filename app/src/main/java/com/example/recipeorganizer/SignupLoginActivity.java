9f6545c20ae9b0a784a7728e4b6c49ce6153b4df
VivianUng <vivung1110@gmail.com>
2024-11-07 18:19:53 +0800

    removed unused import

--- app/src/main/java/com/example/recipeorganizer/SignupLoginActivity.java
+++ app/src/main/java/com/example/recipeorganizer/SignupLoginActivity.java
@@ -3,7 +3,6 @@ package com.example.recipeorganizer;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
